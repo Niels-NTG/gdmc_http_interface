80acce484b6b1d2353a675c04b8bd69a31b32c87
Niels-NTG <ntg999@gmail.com>
2024-03-29 12:46:32 +0100

    Expanded documentation + renaming

--- buildAll.sh
+++ buildAll.sh
@@ -11,10 +11,10 @@ for d in versionProperties/*; do
   # Clean out the folders, build it, and merge it
   # (We could use "./" to run gradlew, but as it is a shell script im going to be running it with the "sh" command)
   echo "==================== Cleaning workspace to build $version ===================="
-  sh gradlew clean -PmcVer=$version --no-daemon || true
+  sh gradlew clean -PtargetMinecraftVersion=$version --no-daemon || true
   echo "====================Building $version ===================="
-  sh gradlew build -PmcVer=$version --no-daemon || true
+  sh gradlew build -PtargetMinecraftVersion=$version --no-daemon || true
   echo "====================Publishing $version ===================="
-  sh gradlew publish -PmcVer=$version --no-daemon || true
+  sh gradlew publish -PtargetMinecraftVersion=$version --no-daemon || true
   # The "| true" at the end of those are just to make sure the script continues even if a build fails
 done
